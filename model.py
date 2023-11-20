c46b76ae0ef90d0d8dd1bfa82904b2c40becaf7d
Deep-unlearning <58599908+Deep-unlearning@users.noreply.github.com>
2023-11-20 21:08:50 +0100

    Add files via upload

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         # Use pretrained ImageNet model
-        self.resnet50 = models.resnet50(pretrained=True)
+        self.resnet50 = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V2)
         # Replace last layer
         num_ftrs = self.resnet50.fc.in_features
         self.resnet50.fc = nn.Linear(num_ftrs, nclasses)
