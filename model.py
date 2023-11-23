d11d0ae80e46dd62e01569ee18bfd10b72067f0e
Deep-unlearning <58599908+Deep-unlearning@users.noreply.github.com>
2023-11-23 23:22:31 +0100

    Add files via upload

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         # Load the pre-trained EfficientNet_V2_S model
-        self.model = models.efficientnet_v2_s(pretrained=True)
+        self.model = models.efficientnet_b4(weights='DEFAULT')
         # Modify the classifier for the number of classes in your dataset
         self.model.classifier[1] = torch.nn.Linear(self.model.classifier[1].in_features, nclasses)
 
