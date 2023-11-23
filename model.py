fcbc4714df1541e6b480bdcd90fa230d9efa70b6
Deep-unlearning <58599908+Deep-unlearning@users.noreply.github.com>
2023-11-23 19:44:50 +0100

    Add files via upload

--- model.py
+++ model.py
@@ -9,22 +9,17 @@ nclasses = 250
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        # Use pretrained EfficientNet model
-        self.efficientnet = models.efficientnet_v2_m(weights='DEFAULT')
+        # Use pretrained ImageNet model
+        self.resnet50 = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V2)
+        # Replace last layer
+        num_ftrs = self.resnet50.fc.in_features
+        self.resnet50.fc = nn.Linear(num_ftrs, nclasses)
 
-        # Get the number of input features of the original classifier
-        num_features = self.efficientnet.classifier[1].in_features
-
-        # Add dropout before the final fully connected layer
-        self.dropout = nn.Dropout(0.2)  # You can adjust the dropout rate as needed
-
-        # Replace the classifier with a new one (dropout + linear layer)
-        self.efficientnet.classifier = nn.Sequential(
-            self.dropout,
-            nn.Linear(num_features, nclasses)
-        )
 
     def forward(self, x):
-        # Pass x through the EfficientNet model
-        x = self.efficientnet(x)
+        x = self.resnet50(x)
+        # add dropout layer
+        x = F.dropout(x, p=0.2, training=self.training)
+        # add softmax activation layer
+        x = F.log_softmax(x, dim=1)
         return x
