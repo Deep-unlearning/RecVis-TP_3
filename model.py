69088b2d33d7e7b052d81a428a61c3472c3d4f2c
Deep-unlearning <58599908+Deep-unlearning@users.noreply.github.com>
2023-11-24 11:11:54 +0100

    Update model.py

--- model.py
+++ model.py
@@ -12,6 +12,14 @@ class Net(nn.Module):
         # Load the pre-trained EfficientNet_V2_S model
         self.model = models.efficientnet_b4(weights='DEFAULT')
         # Modify the classifier for the number of classes in your dataset
+        # Define the layer up to which you want to freeze
+        freeze_until = 6  # For example, freeze up to the end of block 6 in 'features'
+        
+        # Freeze layers in 'features' up to the specified block
+        for name, parameter in model.named_parameters():
+            if name.split('.')[1].isdigit() and int(name.split('.')[1]) < freeze_until:
+                parameter.requires_grad = False
+                
         self.model.classifier[1] = torch.nn.Linear(self.model.classifier[1].in_features, nclasses)
 
     def forward(self, x):
