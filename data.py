a0a5bdd90b849a231c3d6c1a7e457bde108461e3
Deep-unlearning <58599908+Deep-unlearning@users.noreply.github.com>
2023-11-24 10:09:26 +0100

    Update data.py

--- data.py
+++ data.py
@@ -11,3 +11,15 @@ data_transforms = transforms.Compose([
         std=[0.229, 0.224, 0.225]
     )
 ])
+
+data_transforms_train = transforms.Compose([
+    transforms.Resize((384, 384)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(15),
+    transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+])
